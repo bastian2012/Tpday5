ec79790507ce3b87992cfad6b66942c48de30039
Jean ritchy Bastien <96450547+bastian2012@users.noreply.github.com>
2023-08-10 21:41:43 +0000

    Update pitit1.css and pitit1.js

--- src/component/pitit1.js
+++ src/component/pitit1.js
@@ -10,15 +10,16 @@ const Pitit1 = () => {
             </header>
             <section style={{ fontSize: "20px", color: "blue" }}>
                 <div className="postyo">
-                    <h2>competence mw </h2>
+                    <h2 style={{color:'black'}}>competence mw </h2>
                     fpoihbeuotihebcery niefhewioy ehtfewktyu ieuthfeit eihteifther
                 </div>
                 <div className="postyo">
-                    <h2>kalitem</h2>
-                    <p></p>
-                </div>iopfhkoh ofhnmoptr opercpwy peohwpoy operthpowy 9wet,-ye epfwy rfwtyu twyt 
+                    <h2 style={{color:'black'}}>kalitem</h2>
+                        <p>opfhkoh ofhnmoptr opercpwy peohwpoy operthpowy 9wet,-ye epfwy rfwtyu twyt </p>
+                    </div>
+
                 <div className="postyo">
-                    <h2>eperience</h2>
+                    <h2 style={{color:'black'}}>eperience</h2>
                     <p>rtyuivhk chytui vbnfhk fhcvh vbnfhk cvhkfui cvbnui .</p>
                 </div>
             </section>
