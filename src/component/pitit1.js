d201c9e2767bd05eca5c0cd25a05b27467fccdae
Jean ritchy Bastien <96450547+bastian2012@users.noreply.github.com>
2023-08-10 21:56:42 +0000

    Update pitit1.js

--- src/component/pitit1.js
+++ src/component/pitit1.js
@@ -11,16 +11,28 @@ const Pitit1 = () => {
             <section style={{ fontSize: "20px", color: "blue" }}>
                 <div className="postyo">
                     <h2 style={{color:'black'}}>competence mw </h2>
-                    fpoihbeuotihebcery niefhewioy ehtfewktyu ieuthfeit eihteifther
+                    Kounye a, mwen gen konesans ak kapasite nan domèn enformatik,
+                     ak konpetans nan kèk langaj pwogramasyon tankou javascript, react js Python ak
+                      Java. Mwen kapab devlope aplikasyon ki sipòte entèn
+                       entegrasyon ak entèfas diferan. Mwen menm tou kòwòdone
+                        avèk ekipe, epi mwen konprann metòd devlopman agil yo.
                 </div>
                 <div className="postyo">
                     <h2 style={{color:'black'}}>kalitem</h2>
-                        <p>Mwen renmen fè analiz, kapasite pou rezoud pwoblèm, ak kapasite pou kreye kontni ki klè ak byen struktire. Nan tan lib, mwen renmen li, ekri sou divès sijè, ak menm tou aprann sou kilti ak lang diferan yo." </p>
+                        <p>Mwen renmen fè analiz, kapasite pou rezoud pwoblèm, 
+                            ak kapasite pou kreye kontni ki klè ak byen struktire. 
+                            Nan tan lib, mwen renmen li, ekri sou divès sijè,
+                             ak menm tou aprann sou kilti ak lang diferan yo." </p>
                     </div>
 
                 <div className="postyo">
                     <h2 style={{color:'black'}}>eperience</h2>
-                    <p>rtyuivhk chytui vbnfhk fhcvh vbnfhk cvhkfui cvbnui .</p>
+                    <p>Pandan dènye ane yo, mwen te travay nan domèn enformatik
+                         avèk yon konstriksyon imèl. Mwen te devlope aplikasyon ki
+                          sipòte entèn entegrasyon ak entèfas diferan ak mete an
+                           aplikasyon solisyon inovatè. Mwen kòwòdone ak ekipe, 
+                           aplike metòd devlopman agil yo, ak amelyore konesans mwen 
+                           nan Python ak Java.</p>
                 </div>
             </section>
             <footer className="pye">
