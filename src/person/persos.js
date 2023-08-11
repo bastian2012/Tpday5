42b6f7eb2c562204e0076103d1e9d0cd64bddbdd
Jean Ritchy Bastien <bjeanritchy@gmail.com>
2023-08-11 09:10:09 -0400

    creation du projet

---
+++ src/person/persos.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default class Person{
+    personnes = [
+        {
+            nom: "Alice",
+            age: 25,
+            email: "alice@example.com",
+            langue: "Français",
+            pays: "France",
+        },
+        {
+            nom: "Bob",
+            age: 30,
+            email: "bob@example.com",
+            langue: "Anglais",
+            pays: "États-Unis",
+        },
+        
+        {
+            nom: "Eva",
+            age: 28,
+            email: "eva@example.com",
+            langue: "Espagnol",
+            pays: "Espagne",
+        },
+    ];
+
+
+}
